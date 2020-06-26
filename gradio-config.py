38b464cd0f061f7736fc73e291a096dd14859d6f
aliabd <ali.si3luwa@gmail.com>
2020-06-26 10:14:33 -0700

    added slider

--- gradio-config.py
+++ gradio-config.py
@@ -83,7 +83,7 @@ def run_style_transform(style_bottleneck, preprocessed_content_image,
     return stylized_image
 
 
-def predict(content_img, style_img, models):
+def predict(content_img, style_img, content_blending_ratio, models):
     style_predict_path, style_transform_path = models
     content_img, filepath_content = load_img(content_img)
     style_img, filepath_style = load_img(style_img)
@@ -93,7 +93,7 @@ def predict(content_img, style_img, models):
                                          style_predict_path)
     style_bottleneck_content = run_style_predict(
             preprocess_image(content_img, 256), style_predict_path)
-    content_blending_ratio = 0.25
+    # content_blending_ratio = 0.25
     style_bottleneck_blended = content_blending_ratio * \
                                style_bottleneck_content + \
                                (1 - content_blending_ratio) * \
@@ -110,115 +110,15 @@ def predict(content_img, style_img, models):
     return stylized_image_blended
 
 
-INPUTS = [gradio.inputs.ImageIn(cast_to="pillow"), gradio.inputs.ImageIn(
-    cast_to="pillow")]
-OUTPUTS = gradio.outputs.Image()
+INPUTS = [gradio.inputs.ImageIn(cast_to="pillow", label="Content Image"),
+          gradio.inputs.ImageIn(
+    cast_to="pillow", label="Style Image"), gradio.inputs.Slider(0, 1,
+                                                                 "Content "
+                                                                 "Blending "
+                                                                 "Ratio")]
+OUTPUTS = gradio.outputs.Image(label="Stylized Image")
+
 INTERFACE = gradio.Interface(fn=predict, inputs=INPUTS, outputs=OUTPUTS,
                              load_fn=load)
 
-INTERFACE.launch(inbrowser=True)
-
-#
-# Function to load an image from a file, and add a batch dimension.
-# def load_img(path_to_img):
-#   img = tf.io.read_file(path_to_img)
-#   img = tf.io.decode_image(img, channels=3)
-#   img = tf.image.convert_image_dtype(img, tf.float32)
-#   img = img[tf.newaxis, :]
-#
-#   return img
-
-# Function to pre-process by resizing an central cropping it.
-
-# Load the input images.
-# content_image = load_img(content_path)
-# style_image = load_img(style_path)
-
-# Preprocess the input images.
-# # preprocessed_content_image = preprocess_image(content_image, 384)
-# # preprocessed_style_image = preprocess_image(style_image, 256)
-#
-# print('Style Image Shape:', preprocessed_style_image.shape)
-# print('Content Image Shape:', preprocessed_content_image.shape)
-
-
-# def imsave(file, image):
-#     if len(image.shape) > 3:
-#         image = tf.squeeze(image, axis=0)
-#         plt.imsave(file, image)
-#     else:
-#         plt.imsave(file, image)
-
-
-# imsave('results/preproc-content.png', preprocessed_content_image)
-# imsave('results/preproc-style.png', preprocessed_style_image)
-
-# Function to run style prediction on preprocessed style image.
-# def run_style_predict(preprocessed_style_image):
-#     # Load the model.
-#     interpreter = tf.lite.Interpreter(model_path=style_predict_path)
-#
-#     # Set model input.
-#     interpreter.allocate_tensors()
-#     input_details = interpreter.get_input_details()
-#     interpreter.set_tensor(input_details[0]["index"], preprocessed_style_image)
-#
-#     # Calculate style bottleneck.
-#     interpreter.invoke()
-#     style_bottleneck = interpreter.tensor(
-#       interpreter.get_output_details()[0]["index"]
-#       )()
-#
-#     return style_bottleneck
-
-# Calculate style bottleneck for the preprocessed style image.
-# style_bottleneck = run_style_predict(preprocessed_style_image)
-# print('Style Bottleneck Shape:', style_bottleneck.shape)
-
-# Run style transform on preprocessed style image
-# def run_style_transform(style_bottleneck, preprocessed_content_image):
-#   # Load the model.
-#   interpreter = tf.lite.Interpreter(model_path=style_transform_path)
-#
-#   # Set model input.
-#   input_details = interpreter.get_input_details()
-#   interpreter.allocate_tensors()
-#
-#   # Set model inputs.
-#   interpreter.set_tensor(input_details[0]["index"], preprocessed_content_image)
-#   interpreter.set_tensor(input_details[1]["index"], style_bottleneck)
-#   interpreter.invoke()
-#
-#   # Transform content image.
-#   stylized_image = interpreter.tensor(
-#       interpreter.get_output_details()[0]["index"]
-#       )()
-#
-#   return stylized_image
-
-# Stylize the content image using the style bottleneck.
-# stylized_image = run_style_transform(style_bottleneck, preprocessed_content_image)
-
-# Visualize the output.
-# imsave('results/stylized-image.png', stylized_image)
-
-# Calculate style bottleneck of the content image.
-# style_bottleneck_content = run_style_predict(
-#     preprocess_image(content_image, 256)
-#     )
-
-# Define content blending ratio between [0..1].
-# 0.0: 0% style extracts from content image.
-# 1.0: 100% style extracted from content image.
-# content_blending_ratio = 0.5
-
-# # Blend the style bottleneck of style image and content image
-# style_bottleneck_blended = content_blending_ratio * style_bottleneck_content \
-#                            + (1 - content_blending_ratio) * style_bottleneck
-#
-# # Stylize the content image using the style bottleneck.
-# stylized_image_blended = run_style_transform(style_bottleneck_blended,
-#                                              preprocessed_content_image)
-
-# Visualize the output.
-# imsave('results/stylized-image-blended.png', stylized_image_blended)
+INTERFACE.launch(inbrowser=True, share=True)
